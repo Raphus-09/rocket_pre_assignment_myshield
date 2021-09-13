ff3481cd7ef3adf0fe1c9dbf67ef4620eebd6dad
Raphus-09 <65695437+Raphus-09@users.noreply.github.com>
2021-09-14 00:48:08 +0900

    Add files via upload

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject square;
+    public GameObject gameOverText;
+
+    public Text timeText;
+    float alive = 0.0f;
+
+    public static gameManager I;
+
+    public Animator anim;
+    public GameObject balloon;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        I = this;
+    }
+
+    void Start()
+    {
+        InvokeRepeating("makeSquare", 0.0f, 0.5f);
+    }
+
+    void makeSquare()
+    {
+        Instantiate(square);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alive += Time.deltaTime;
+        timeText.text = alive.ToString("N2");
+    }
+
+    public void gameOver()
+    {
+        anim.SetBool("isDie", true);
+        gameOverText.SetActive(true);
+        Invoke("dead", 0.5f);
+    }
+
+    void dead()
+    {
+        Time.timeScale = 0.0f;
+        Destroy(balloon);
+    }
+}
